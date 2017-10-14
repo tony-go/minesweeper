3a8039718e5bbe589ff32d144c15b6753408fb4e
Tony Gorez <gorez.tony@gmail.com>
2017-10-14 16:39:23 +0200

    part 3

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,19 +1,57 @@
-const printBoard = (board) => {
-  console.log('Current Board:');
-  console.log(board[0].join(' | '));
-  console.log(board[1].join(' | '));
-  console.log(board[2].join(' | '));
+// PLAYER BOARD
+
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  let board = [];
+
+  for (let myRows = 0; myRows < numberOfRows ; myRows ++) {
+    let row = [];
+    for (let myColumns = 0; myColumns < numberOfColumns ; myColumns ++) {
+      row.push(' ');
+    };
+    board.push(row);
+  };
+  return board;
 };
 
-let board = [
-  [' ',' ',' '],
-  [' ',' ',' '],
-  [' ',' ',' ']
-];
+//BOMB BOARD
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  let board = [];
 
-printBoard(board);
+  for (let myRows = 0; myRows < numberOfRows ; myRows ++) {
+    let row = [];
+    for (let myColumns = 0; myColumns < numberOfColumns ; myColumns ++) {
+      row.push(null);
+    };
+    board.push(row);
+  };
+  //return board;
 
-board[0][1] = '1';
-board[2][2] = 'B';
+  let numberOfBombsPlaced = 0;
 
-printBoard(board);
+  while (numberOfBombsPlaced < numberOfBombs) {
+    // what if two index are the same ?
+    let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    let randomColumnsIndex = Math.floor(Math.random() * numberOfColumns);
+
+    board[randomRowIndex][randomColumnsIndex] = 'B';
+
+    numberOfBombsPlaced++;
+  };
+  return board;
+};
+
+// PRINT BOARD
+const printBoard = (board) => {
+  return board.map(row => row.join('|')).join('\n');
+};
+
+let playerBoard = generatePlayerBoard(3, 4);
+let bombBoard = generateBombBoard(3, 4, 5);
+
+
+
+// OUTPUT
+console.log('Player Board: ');
+console.log(printBoard(playerBoard));
+console.log('Bomb Board: ');
+console.log(printBoard(bombBoard));
